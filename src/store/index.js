b7ff093e616d4aa193681206e74adf23ef94b09b
jixiaotai <jixt@akulaku.com>
2022-10-09 11:02:28 +0800

    注释

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,11 @@ import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import thunk from "redux-thunk";
 import reducer from "./reducer";
 
+/**
+ * 中间件
+ * 实现了从 action => middleware => reducer 数据流的改变
+ * 可以实现 异步action、 action过滤、错误日志...
+ */
 const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
